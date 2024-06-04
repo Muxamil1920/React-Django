7d4c7ee16e1ac71d99cd638ae86a4ea350979909
Mr Muzamil <muxamil1920@gmail.com>
2024-06-04 23:47:38 +0530

    firstcommit

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class React(models.Model):
+    employee = models.CharField(max_length=50),
+    department = models.CharField(max_length=200)
